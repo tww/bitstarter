9b161d5b2732131694590ac31e9993aa3825aef3
Thurston.Watson <thurston.watson@gmail.com>
2013-07-11 06:33:10 +0000

    removed . on file path

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-     fs.readFile('./index.html', function (err, data) {
+     fs.readFile('/index.html', function (err, data) {
          if (err) throw err;
          
          response.send(data.toString('utf8', 0, data.length);
