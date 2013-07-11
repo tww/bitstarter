5dc227a7e863668fdb8abf904693f6b8d1052f52
Thurston.Watson <thurston.watson@gmail.com>
2013-07-11 06:07:58 +0000

    Added capability to read the index.html file and return in the response

--- web.js
+++ web.js
@@ -3,7 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+     fs.readFile('index.html', function (err, data) {
+         if (err) throw err;
+         
+         response.send(data.toString('utf8', 0, data.length);
+
+//         response.send('Hello World2!');
+     }); 
+   
 });
 
 var port = process.env.PORT || 5000;
